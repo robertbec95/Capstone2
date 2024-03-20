5b15eb6f8df3652edf667b45f7a4e439388878f3
robertbec95 <robert_becsenescu@yahoo.com>
2024-03-20 19:38:11 +0100

    updated

--- back/main.py
+++ back/main.py
@@ -2,38 +2,44 @@ import json
 import requests
 from flask import Flask, request, jsonify, session
 from flask_cors import CORS
-import models as md
+#import custom_functions as cp # custom module
+from models import db, User, Portfolio
 import os
+from flask_sqlalchemy import SQLAlchemy
 import oracledb
-from sqlalchemy.pool import StaticPool
-from dotenv import load_dotenv # For retrieving SECRETS
+from sqlalchemy.pool import NullPool
+#from dotenv import load_dotenv # For retrieving SECRETS
 import secrets
+from werkzeug.security import generate_password_hash, check_password_hash
+
+#db = SQLAlchemy()
 
 # Load environment variables from .env file
-load_dotenv()
+#load_dotenv()
 
 # Flask setup
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
-app.config["SECRET_KEY"] = cp.get_flask_secret()
+app.config["SECRET_KEY"] = secrets.token_hex(16)
+#db = md.db
 
 # Database setup
-md.config_flask_db(app)
+#db.init_app(app)
 
 # Oracle credentials
-un = os.getenv("ADMIN")
-pw = os.getenv("Capstone1!")
-dsn = os.getenv('d9x7y23dgzbr0azz_high')
 
-# Use environment variables in SQLALCHEMY_DATABASE_URI
-app.config['SQLALCHEMY_DATABASE_URI'] = f'oracle+oracledb://{un}:{pw}@{dsn}'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-# Use StaticPool as an example, consider your app's needs for pool configuration
+un = "ADMIN"
+pw = "Capstoneproject123"
+dsn = "(description= (retry_count=20)(retry_delay=3)(address=(protocol=tcps)(port=1521)(host=adb.eu-madrid-1.oraclecloud.com))(connect_data=(service_name=g4bbbc586754471_d9x7y23dgzbr0azz_high.adb.oraclecloud.com))(security=(ssl_server_dn_match=yes)))"
+pool = oracledb.create_pool(user=un, password=pw, dsn=dsn)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'oracle+oracledb://'
+#{un}:{pw}@{dsn}'
 app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
-    'poolclass': StaticPool
+    'creator': pool.acquire,
+    'poolclass': NullPool
 }
-app.config['SQLALCHEMY_ECHO'] = True 
-db = md.db # Make sure you import and initialize your SQLAlchemy instance correctly
+app.config['SQLALCHEMY_ECHO'] = True
 db.init_app(app)
 
 with app.app_context():
@@ -61,11 +67,11 @@ def get_portfolio_summary():
     portfolio = {}
     portfolio["username"] = user
     total_port_val = 0
-    user_portfolio = md.get_portfolio(username=user)
+    user_portfolio = db.get_portfolio(username=user)
 
     portfolio["portfolio"] = {}
     for stock, num_stocks in user_portfolio.items():
-        data = cp.get_past_vals(stock, "min60")
+        data = cp.get_past_vals(stock, "1d")
         try:
             last_close = cp.get_last_close(data)
         except:
@@ -141,11 +147,6 @@ def buy():
 
 @app.route("/sell", methods=['POST'])
 def sell_function():
-    data = request.get_json()
-    username = session["username"]
-    stock = data["stock"]
-    quantity = data["quantity"]
-
     data = request.get_json()
     username = session["username"]
     stock = data["stock"]
